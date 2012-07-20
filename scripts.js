f1a3cbf4e33a1a23f03647e7e70f5c26a8435397
Michael Martin <michaelmartinmail@gmail.com>
2012-07-19 17:24:12 -0700

    QAd
    Added comments; tested game

--- scripts.js
+++ scripts.js
@@ -1,70 +1,80 @@
 function setupPage() {
-    $(document).ready(function() {
-        $("#newGame").click( function(event) {
-            $("#lostMessage").addClass("hidden");
+    $(document).ready(function() {  // page is ready
+        $("#newGame").click( function(event) {  // handle user clicking on "New Game"
+            $("#lostMessage").addClass("hidden");   // Hide win/lose messages from last game
             $("#wonMessage").addClass("hidden");
             
-            var gameStatus = "playing";
-            var gameBoard = new board(8, 10);
+            var gameStatus = "playing";             
+            var gameBoard = new board(8, 10);       // create new board
             var boardHtml = gameBoard.boardHTML;
             
-            if( $("#board").length == 0 ){
+            if( $("#board").length == 0 ){          // insert board if first game
                 $("#boardWrapper").append(boardHtml);
             }
             else {
-             $("#board").replaceWith(boardHtml);
+             $("#board").replaceWith(boardHtml);    // otherwise replace the old one
             }
             
-            $("td.unclicked").click( function(event) {
+            $("td.unclicked").click( function(event) {  // bind click events to all tiles
                 if( gameStatus == "playing" ) {
-                    var tileId = $(this).attr('id');
+                    var tileId = $(this).attr('id');    // grab id from tile clicked
                     var xPos = tileId.split("_")[0];
                     var yPos = tileId.split("_")[1];
-                    var clickedTile = getTileFromBoard(gameBoard, xPos, yPos);
+                    var clickedTile = getTileFromBoard(gameBoard, xPos, yPos);  // use it to get the relevant tile object
                     
-                    clickTile(xPos, yPos, gameBoard);
-                    gameStatus = getGameStatus(clickedTile);
+                    clickTile(xPos, yPos, gameBoard);       // and click the tile
+                    gameStatus = getGameStatus(clickedTile);    // check if we've won or lost
                     
-                    if( gameStatus == "lost" ) {
-                        $("#lostMessage.hidden").removeClass("hidden");
+                    if( gameStatus == "lost" ) {                // if we've lost...
+                        $("#lostMessage.hidden").removeClass("hidden"); // unhide losing message element :(
                     }
                 }
             });
             
-            $("#validate").click( function(event) {
-                gameStatus = checkIfBoardIsWinner(gameBoard);
+            $("#validate").click( function(event) {    // handle user clicking "validate" during game
+                gameStatus = checkIfBoardIsWinner(gameBoard);   //see if they've won or lost
                 
                 if( gameStatus == "lost" ) {
-                    $("#lostMessage.hidden").removeClass("hidden");
+                    $("#lostMessage.hidden").removeClass("hidden"); // display lost message
                 }
                 else if( gameStatus == "won" ) {
-                    $("#wonMessage.hidden").removeClass("hidden");
+                    $("#wonMessage.hidden").removeClass("hidden"); // display won message!
                 }
             });
         });
         $("#cheat").click( function(event) {
-            $(".bomb").addClass("cheating");
+            $(".bomb").addClass("cheating");    // apply cheating class to tile elements; made red by CSS
         });
         
     });
 }
 
-function board(size, difficulty) { // how to incorporate OOP?
+
+// BOARD OBJECT
+// boardArray: a 2d array of Tile objects
+// boardHTML: initial HTML representation of the board
+function board(size, difficulty) { // board object
     this.size = size;
     this.difficulty = difficulty;
     
-    var initialBoardArray = buildBoardArray(this, size);
-    this.boardArray = populateBombs(initialBoardArray, difficulty);
-    this.boardHTML = buildBoardHTML(this);
+    var initialBoardArray = buildBoardArray(this, size);    // build blank board
+    this.boardArray = populateBombs(initialBoardArray, difficulty); // add bombs for good measure
+    this.boardHTML = buildBoardHTML(this);  // build HTML representation of board
 }
 
+// buildBoardArray
+// input:
+//          parentBoard - Board object
+//          size - length of one side of board
+// returns:
+//          a 2d array of Tile objects
 function buildBoardArray(parentBoard, size) {
     var boardArray = new Array(size);
     for( var i=0; i<size; i++ ) {
         boardArray[i] = new Array(size);
     }
     
-    for( var i=0; i<size; i++ ) {
+    for( var i=0; i<size; i++ ) {       // initially populate board entirely with blanks
         for( var j=0; j<size; j++ ) {
             boardArray[i][j] = new tile(i, j, "blank", "unclicked", parentBoard);
         }
@@ -73,27 +83,41 @@ function buildBoardArray(parentBoard, size) {
     return boardArray;
 }
 
+// populateBombs
+// input:
+//          boardArray - 2d array of Tile objects
+//          difficulty - number of tiles to be placed (MUST BE LESS THAN TOTAL SIZE OF BOARDARRAY)
+// returns:
+//          boardArray - the input array, but now some tiles have bombs in them!
 function populateBombs(boardArray, difficulty) {
     
-    for( var i=0; i<difficulty; i++ ) {
-        var convertedBombTile = convertRandomBombTile(boardArray);
+    for( var i=0; i<difficulty; i++ ) {     // iterate over your "pile" of bombs
+        var convertedBombTile = convertRandomBombTile(boardArray);  // get back a random Tile that's been converted to a bomb tile
         var x = convertedBombTile.x;
         var y = convertedBombTile.y;
         
-        boardArray[x][y] = convertedBombTile;
+        boardArray[x][y] = convertedBombTile;   // and displace the current tile
     }
     
     return boardArray;
 }
 
+// convertRandomBombTile (recursive)
+// input:
+//          boardArray - a 2d array of Tile objects
+// returns:
+//          a Tile that has been converted to a bomb tile
+// note:
+//          may never return if it keeps randomly picking a bomb tile
+//          runtime increases non-linearly with difficulty
 function convertRandomBombTile(boardArray) {
-    xPos = Math.floor(Math.random() * boardArray.length);
+    xPos = Math.floor(Math.random() * boardArray.length);   // pick a tile at random
     yPos = Math.floor(Math.random() * boardArray.length);
     
     var pickedTile = boardArray[xPos][yPos];
     var tileType = pickedTile.type;
     
-    if( tileType == "bomb" ) {
+    if( tileType == "bomb" ) {      // if that tile is already a bomb, pick again
         return convertRandomBombTile(boardArray);
     }
     else {
@@ -102,14 +126,19 @@ function convertRandomBombTile(boardArray) {
     }
 }
 
+// buildBoardHTML
+// input:
+//          parentBoard - a Board object
+// returns:
+//          myBoardHTML - a string representing the HTML representation of the parentBoard input; HTML table-based
 function buildBoardHTML(parentBoard) {
-    var boardArray = parentBoard.boardArray;
+    var boardArray = parentBoard.boardArray;    // grab the boardArray from parentBoard
     
     var myBoardHTML = "";
     
-    myBoardHTML += "<table id='board' border='1'>";
+    myBoardHTML += "<table id='board' border='1'>"; // start building an HTML table
     
-    for( var i=0; i<boardArray.length; i++ ) {
+    for( var i=0; i<boardArray.length; i++ ) {      // iterate over all of the boardArray
         myBoardHTML += "<tr>";
         for( var j=0; j<boardArray.length; j++ ) {
             var currentTile = boardArray[i][j];
@@ -130,11 +159,24 @@ function buildBoardHTML(parentBoard) {
     return myBoardHTML;
 }
 
+// getTileFromBoard
+// inputs:
+//          parentBoard -- a Board object
+//          x -- number: x position of desired Tile
+//          y -- number: y position of desired Tile
+// returns:
+//          the Tile object at the specified location
 function getTileFromBoard( parentBoard, x, y ) {
     return parentBoard.boardArray[x][y];
 }
 
-function tile(x, y, type, status, parentBoard) { // how to incorporate OOP?
+// Tile object
+// x -- x position of Tile
+// y -- y position of Tile
+// type: "blank" or "bomb"
+// status: "clicked" or "unclicked"
+// parentBoard: the Board object that this tile is a part of
+function tile(x, y, type, status, parentBoard) { 
     this.x = x;
     this.y = y;
     this.type = type;
@@ -142,6 +184,12 @@ function tile(x, y, type, status, parentBoard) { // how to incorporate OOP?
     this.bombCount = 0;
 }
 
+// countNeighborBombs
+// inputs:
+//          tile: a Tile object for which you wish to find the number of neighboring bombs
+//          parentBoard: the Board object where the tile lives (not necessary?)
+// returns:
+//          bombCount -- a number: number of neighboring bombs to this tile
 function countNeighborBombs(tile, parentBoard) {
     var bombCount = 0;
     
@@ -150,7 +198,7 @@ function countNeighborBombs(tile, parentBoard) {
     
     var boardArray = parentBoard.boardArray;
     
-    for( var i=-1; i<=1; i++ ) {
+    for( var i=-1; i<=1; i++ ) {        //iterate over all neighbors
          for( var j=-1; j<=1; j++ ) {
             if( !(i == 0 && j == 0) && (x+i) >=0 && (y+j) >=0 && (x+i) < boardArray.length && (y+j) < boardArray.length ) {    // don't look at self, don't look at out of bounds
                 var neighborTile = boardArray[x+i][y+j];
@@ -165,13 +213,19 @@ function countNeighborBombs(tile, parentBoard) {
     return bombCount;
 }
 
+// clickTile
+// inputs:
+//          x -- x position of Tile to click
+//          y -- y position of Tile to click
+//          parentBoard -- the Board object associated with the tile to be clicked
+// clicks a tile by adjusting the HTML on the page largely through jQuery
 function clickTile(x, y, parentBoard) {
     var clickedTile = getTileFromBoard(parentBoard, x, y);
-    var bombCount = clickedTile.bombCount;
+    var bombCount = clickedTile.bombCount;      // get the bombCount for this tile
     var tileType = clickedTile.type;
-    clickedTile.status = "clicked";
+    clickedTile.status = "clicked"; 
     
-    $("#"+x+"_"+y).addClass("clicked");
+    $("#"+x+"_"+y).addClass("clicked");         // adjust HTML
     $("#"+x+"_"+y).removeClass("unclicked");
     if( tileType == "blank" ) {
         $("#"+x+"_"+y).html(bombCount);
@@ -181,6 +235,12 @@ function clickTile(x, y, parentBoard) {
     }
 }
 
+// getGameStatus
+// inputs:
+//          tile -- a Tile object (just clicked on)
+// returns:
+//          "lost" -- the Tile is a bomb type with a clicked status
+//          "playing" -- the Tile is a blank
 function getGameStatus(tile) {
     var type = tile.type;
     var status = tile.status;
@@ -193,6 +253,13 @@ function getGameStatus(tile) {
     }
 }
 
+// checkIfBoardIsWinner
+// inputs:
+//          board -- a Board object
+// returns:
+//          "lost" -- user has not clicked on all blank tiles
+//          "won" -- all blank tiles clicked
+// this is called when user validates game
 function checkIfBoardIsWinner(board) {
     var boardArray = board.boardArray;
     
